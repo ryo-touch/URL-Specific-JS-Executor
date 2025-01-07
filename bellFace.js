c34ea458757cab76f67f69bfcac305b69e77baad
ryo_touch <ryo.sakai@speee.jp>
2025-01-07 17:40:16 +0900

    bellfaceのログ表示をいい感じにする

---
+++ bellFace.js
@@ -0,0 +1,6 @@
+(function() {
+  document.querySelector("#Information").remove();
+  document.querySelector('.video_wrapper').style.marginLeft = '0px';
+  document.querySelector('.video_wrapper').style.width = '100%';
+  console.log('JavaScript executed');
+})();
